1a16482cddc2eb4ad3d0ce14f29e197ccbc87cac
Rahafhosari <rahaf.hussary@gmail.com>
2021-02-02 15:28:53 +0200

    Axios Pokemon API

--- React/api/src/App.js
+++ React/api/src/App.js
@@ -2,12 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 import Pokemon from './components/Pokemon';
+import AxiosPokemon from './components/AxiosPokemon';
 
 function App() {
   return (
+    <>
     <div className="App">
-      <Pokemon />
+      <Pokemon />    
     </div>
+    <div>
+    <AxiosPokemon />
+    </div>
+    </>
   );
 }
 
