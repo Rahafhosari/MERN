c6450f16a7585249128346f345523b6167bbaebf
Rahafhosari <rahaf.hussary@gmail.com>
2021-02-02 12:32:35 +0200

    Pokemon

--- React/api/src/components/Pokemon.js
+++ React/api/src/components/Pokemon.js
@@ -3,14 +3,14 @@ import React, {useEffect, useState} from 'react'
 const Pokemon = () => {
     const [pokemon, setPokemon] = useState([]);
 
-    useEffect(() => {
+    const fetchPoke = () => {
         fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
         .then(response => response.json())
         .then(response => setPokemon(response.results))
-    },[pokemon]);
+    }
     return (
-        <div>
-            <button onClick={useEffect}>Fetch Pokemon</button>
+        <div className="box">
+            <button onClick={fetchPoke} className="apiFetch">Fetch Pokemon</button>
             {pokemon.length > 0 && pokemon.map((pokemon, index)=>(<div key={index}>Index: {index} | Pokemon Name: "{pokemon.name}"</div>))}
         </div>
     );
