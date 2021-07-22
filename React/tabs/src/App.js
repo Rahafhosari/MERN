605ee1796abe6128511f5a90aa4eb0a479fcc188
Rahafmh <rahaf.hussary@gmail.com>
2021-07-22 12:08:32 +0300

    Tabs + JQuery

--- React/tabs/src/App.js
+++ React/tabs/src/App.js
@@ -2,15 +2,14 @@ import React, { useState } from 'react';
 import './App.css';
 import Tabs  from './Components/Tabs';
 import TabsDisplay  from './Components/TabsDisplay';
-
+import $ from 'jquery';
 
 function App() {
   const [currentTab, setcurrentTab] = useState("");
   
   const newTabContent = (content) => {
-  setcurrentTab(content);
+    setcurrentTab(content);
 }
-
   return (
     <div className="App">
         <h1>Tabs</h1>
--- React/tabs/src/Components/TabsDisplay.js
+++ React/tabs/src/Components/TabsDisplay.js
@@ -1,10 +1,28 @@
 import react, { useState } from 'react';
+import $ from 'jquery';
 
 const TabsDisplay = (props) => {
+    const tabStyle = {
+        border: "1px solid",
+    };
+
+    $(".displayShow").css("display", "none");
+    // $(".displayShow").slideDown("meduim");
+    $(".displayShow").fadeIn();
+
     return (
-        <div>
-            <p>{ props.value }</p>
-        </div>
+        <>   
+            {/* <div style={tabStyle} className="displayShow">
+                <p >{props.content}</p>
+            </div> */}
+            {props.content 
+            ?
+            <div style={tabStyle} className="displayShow">
+                <p >{props.content}</p>
+            </div>
+            : <div></div>
+            }
+        </>
     );
 };
 
